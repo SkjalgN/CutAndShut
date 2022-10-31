09594cf5fdc310e06e0df119c64d2f6482930988
andreasolborg <andreas.olborg@hotmail.no>
2022-10-31 13:14:43 +0100

    Added reservations component

--- main.js
+++ main.js
@@ -1,2 +1,4 @@
 import {Navbar} from "./components/navbar.js";
 import {Footer} from "./components/footer.js";
+/* import Reservations from "./components/reservations.js"; */
+import {Reservations} from "./components/reservations.js";
