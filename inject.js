c3d3a5835de1e4928428cc71930ddf5940f099c3
andreasolborg <andreas.olborg@hotmail.no>
2022-11-10 15:00:47 +0100

    added jons modulairty to base pages

---
+++ inject.js
@@ -0,0 +1,41 @@
+var navbarElement = document.querySelector('#navbar');
+var navbarTemplate = `
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<div class="topnav" id="myTopnav">
+  <div class="logo">
+    <a href="index.html"><img src="images/logo.png" alt="logo" class="logoImage"></a>
+  </div>
+  <div class="menu">
+      <a href="index.html" id="navbarOption">Hjem</a>
+      <a href="gallery.html" id="navbarOption" >Galleri</a>
+      <a href="about.html" id="navbarOption">Om oss</a>
+      <a href="reservations.html" id="navbarOption">Timebestilling</a>
+      <a href="contact.html" id="navbarOption">Kontakt</a>
+      <a href="javascript:void(0);" class="icon">
+        <i class="fa fa-bars"></i>
+      </a>
+      </div>
+</div>
+`
+
+navbarElement.innerHTML = navbarTemplate;
+
+var footerElement = document.querySelector('#footer');
+var footerTemplate = `
+<div class="footer">
+      <div class="footerContent">
+        <div class="footerLogo
+            <a href="index.html"><img src="images/logo.png" alt="logo" class="logoImage"></a>
+        </div>
+        <div class="footerContact">
+            <h2>Kontakt oss</h2>
+            <p>Telefon: 12345678</p>
+            <p>E-post: <a href="mailto:
+        </div>
+      </div>
+    </div>
+`
+
+footerElement.innerHTML = footerTemplate;
+
