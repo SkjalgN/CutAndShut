4e903453a4a85fb0d1d3a5d1cff038470b2452ff
Ole <oleevca@gmail.com>
2022-11-21 21:27:43 +0100

    Lagt til rotate logo i egen js fil

--- webpage/inject.js
+++ webpage/inject.js
@@ -35,25 +35,6 @@ var navbarTemplate = `
   </div>
 `
 
-var looper;
-var degrees = 0;
-function rotate(el, speed) {
-  var elem = document.getElementById("logopic");
-  elem.style.transform = "rotate(" + degrees + "deg)";
-  looper = setTimeout('rotate(\'' + el + '\',' + speed + ')', speed);
-  degrees++;
-  if (degrees > 359) {
-    clearTimeout(looper);
-    degrees = 0;
-  }
-}
-
-function mouseOn(element) {
-  rotate("logopic", 5);
-}
-
-
-
 navbarElement.innerHTML = navbarTemplate;
 
 var footerElement = document.querySelector('.footer');
